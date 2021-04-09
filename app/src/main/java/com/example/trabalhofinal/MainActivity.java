68fa9bf41f5ad8ce540d8284aa29f1e0cb9fcc88
moraesAN <moraes1945@gmail.com>
2021-04-08 22:55:57 -0300

    commit

--- app/src/main/java/com/example/trabalhofinal/MainActivity.java
+++ app/src/main/java/com/example/trabalhofinal/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 /*
 *
 *                      @@@@@@@@@@@@@@@@ Nathalia Moraes @@@@@@@@@@@@@@@@@@@
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtResultadoIMC, txtPesoIdeal;
     private ImageView imgIMC;
     private TextToSpeech tts;
-    private AnimationUtils animationUtils;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,37 +40,61 @@ public class MainActivity extends AppCompatActivity {
         txtResultadoIMC = findViewById(R.id.txtResultadoIMC);
         imgIMC = findViewById(R.id.imgIMC);
         txtPesoIdeal = findViewById(R.id.txtPesoIdeal);
+        btnCalcular = findViewById(R.id.btnCalcular);
 
         edtAltura.addTextChangedListener(new MaskWatcher("#.##"));
+
+        edtAltura.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    btnCalcular.performClick();
+                    ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                            .hideSoftInputFromWindow(edtAltura.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
     public void calcular(View view){
-        double result = Imc.calcularIMC(Double.parseDouble(edtPeso.getText().toString()), Double.parseDouble(edtAltura.getText().toString()));
 
-        if (result >= 18.9 && result <= 24.9){
-            txtResultadoIMC.setText(getString(R.string.imcNormal)+" "+String.format("%.1f", result)+"Kg/m²");
-            imgIMC.setBackgroundResource(R.drawable.imcnormal);
-            imgIMC.setVisibility(View.VISIBLE);
-        }else if (result >= 25.0 && result <= 29.9){
-            txtResultadoIMC.setText(getString(R.string.imcSobrepeso)+" "+ String.format("%.1f", result)+"Kg/m²");
-            imgIMC.setVisibility(View.VISIBLE);
-            imgIMC.setBackgroundResource(R.drawable.imcacima);
-        }else if (result >= 30.0 && result <= 39.9){
-            txtResultadoIMC.setText(getString(R.string.imcObeso)+" "+ String.format("%.1f", result)+"Kg/m²");
-            imgIMC.setVisibility(View.VISIBLE);
-            imgIMC.setBackgroundResource(R.drawable.imcobesidade);
-        }else if (result >= 40.0){
-            txtResultadoIMC.setText(getString(R.string.imcObesoGrave)+" "+ String.format("%.1f", result)+"Kg/m²");
-            imgIMC.setVisibility(View.VISIBLE);
-            imgIMC.setBackgroundResource(R.drawable.imcobesidade3);
-        }else if (result <= 18.8){
-            txtResultadoIMC.setText(getString(R.string.imcMagreza)+" "+ String.format("%.1f", result)+"Kg/m²");
-            imgIMC.setVisibility(View.VISIBLE);
-            imgIMC.setBackgroundResource(R.drawable.imcabaixo);
+        if(edtPeso.getText().toString().isEmpty() || edtAltura.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(this, "Preencha os campos para calcular o IMC", Toast.LENGTH_LONG);
+            TextView v = toast.getView().findViewById(android.R.id.message);
+            v.setTextColor(Color.MAGENTA);
+            toast.show();
+        } else {
+            double result = Imc.calcularIMC(Double.parseDouble(edtPeso.getText().toString()), Double.parseDouble(edtAltura.getText().toString()));
+
+            if (result >= 18.9 && result <= 24.9) {
+                txtResultadoIMC.setText(getString(R.string.imcNormal) + " " + String.format("%.1f", result) + "Kg/m²");
+                imgIMC.setBackgroundResource(R.drawable.imcnormal);
+                imgIMC.setVisibility(View.VISIBLE);
+            } else if (result >= 25.0 && result <= 29.9) {
+                txtResultadoIMC.setText(getString(R.string.imcSobrepeso) + " " + String.format("%.1f", result) + "Kg/m²");
+                imgIMC.setVisibility(View.VISIBLE);
+                imgIMC.setBackgroundResource(R.drawable.imcacima);
+            } else if (result >= 30.0 && result <= 39.9) {
+                txtResultadoIMC.setText(getString(R.string.imcObeso) + " " + String.format("%.1f", result) + "Kg/m²");
+                imgIMC.setVisibility(View.VISIBLE);
+                imgIMC.setBackgroundResource(R.drawable.imcobesidade);
+            } else if (result >= 40.0) {
+                txtResultadoIMC.setText(getString(R.string.imcObesoGrave) + " " + String.format("%.1f", result) + "Kg/m²");
+                imgIMC.setVisibility(View.VISIBLE);
+                imgIMC.setBackgroundResource(R.drawable.imcobesidade3);
+            } else if (result <= 18.8) {
+                txtResultadoIMC.setText(getString(R.string.imcMagreza) + " " + String.format("%.1f", result) + "Kg/m²");
+                imgIMC.setVisibility(View.VISIBLE);
+                imgIMC.setBackgroundResource(R.drawable.imcabaixo);
+            }
+            txtPesoIdeal.setText(getString(R.string.pesoIdeal) + " " + String.format("%.1f", Imc.pesoIdeal(Double.parseDouble(edtAltura.getText().toString()))) + "Kg/m²");
+            fala(getString(R.string.nota));
+
+            btnCalcular.setOnClickListener(v -> limpaBtn(v));
+            btnCalcular.setText("Limpar");
         }
-        txtPesoIdeal.setText(getString(R.string.pesoIdeal)+" "+String.format("%.1f", Imc.pesoIdeal(Double.parseDouble(edtAltura.getText().toString())))+"Kg/m²");
-        fala(getString(R.string.nota));
     }
     public void fala (String text){
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
@@ -76,6 +105,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void limpaBtn(View view){
+        edtAltura.setText("");
+        edtPeso.setText("");
+        txtPesoIdeal.setText("");
+        txtResultadoIMC.setText("");
+        imgIMC.setBackground(null);
+        imgIMC.setVisibility(View.INVISIBLE);
+        tts.shutdown();
+
+        btnCalcular.setOnClickListener(v -> calcular(v));
+        btnCalcular.setText("Calcular");
+
     }
 
     @Override
