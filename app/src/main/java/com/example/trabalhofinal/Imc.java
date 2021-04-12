2dc7c58658a9941edf9d5465041f6aa7d9ec30ed
moraesAN <moraes1945@gmail.com>
2021-04-12 19:08:29 -0300

    1.0.3

--- app/src/main/java/com/example/trabalhofinal/Imc.java
+++ app/src/main/java/com/example/trabalhofinal/Imc.java
@@ -1,11 +1,8 @@
 package com.example.trabalhofinal;
 
 public class Imc {
-    public static double calcularIMC(double p, double a){
+    public static double calcularIMC(double p, double a){ return p/(Math.pow(a , 2));}
         // p = peso e a = altura
-        return p/(Math.pow(a , 2));
-    }
-
     public static double pesoIdeal(double a){
         return ((a - 100)*0.80)*-1;
     }
